b9f6b2ee1e857d45a742819d1c757c2bb253edbe
Javier <deese2k@gmail.com>
2024-06-21 16:08:53 +0200

    Adding the option to colorize the available services

--- jw.py
+++ jw.py
@@ -9,11 +9,24 @@ from imdb import Cinemagoer
 USE_IMDB = True
 VERBOSE = False
 
+# Edit this to add the services you have available
+AVAILABLE_SERVICES = [ "netflix", "max", "amazon prime video", "filmin", "movistar plus" ]
+
 ia = Cinemagoer()
 
 def vprint(data):
     if VERBOSE:
         print(data)
+
+def colorize_services(offers):
+    ret = []
+    for offer in offers:
+        if any([offer.lower() == i.lower() for i in AVAILABLE_SERVICES]):
+            ret.append(f"[bright_green]{offer}[/bright_green]")
+        else:
+            ret.append(f"{offer}")
+    return ", ".join(ret)
+
 
 def do_query(query, use_imdb=False, lang="ES", country="es", limit=5, year=None):
     vprint(f"Searching for \"{query}\"")
@@ -27,7 +40,7 @@ def do_query(query, use_imdb=False, lang="ES", country="es", limit=5, year=None)
     table.add_column("ID", style="magenta")
     table.add_column("released", style="green")
     table.add_column("score", style="red")
-    table.add_column("offers", style="blue")
+    table.add_column("offers", style="grey66")
 
     for c, result in enumerate(results):
         vprint(f"Parsing result {c}")
@@ -49,7 +62,7 @@ def do_query(query, use_imdb=False, lang="ES", country="es", limit=5, year=None)
             except Expception as e:
                 vprint(f"Error getting IMDB score: {e}")
 
-        table.add_row(result.title, str(result.entry_id), str(result.release_year), str(score), ", ".join(offers))
+        table.add_row(result.title, str(result.entry_id), str(result.release_year), str(score), colorize_services(offers))
 
     if table.row_count:
         print(table)
@@ -60,10 +73,10 @@ def do_query(query, use_imdb=False, lang="ES", country="es", limit=5, year=None)
 parser = argparse.ArgumentParser(description="JustWatch CLI")
 parser.add_argument("query", type=str, help="Search query")
 parser.add_argument("--imdb", action="store_true", help="Use IMDB to get ratings")
-parser.add_argument("--lang", type=str, default="ES", help="Language")
-parser.add_argument("--country", type=str, default="es", help="Country")
-parser.add_argument("--limit", type=int, default=5, help="Limit results")
-parser.add_argument("--year", type=int, help="Year")
+parser.add_argument("--lang", type=str, default="ES", help="Language to use. Defaults to 'es'")
+parser.add_argument("--country", type=str, default="es", help="Country to use. Defaults to 'ES'. ")
+parser.add_argument("--limit", type=int, default=5, help="Limit results to this number")
+parser.add_argument("--year", type=int, help="Year when you think it was release. It will automatically add +5 and -5 years to the number.")
 parser.add_argument("--verbose", action="store_true", help="Verbose output")
 
 args = parser.parse_args()
